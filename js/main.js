7864486d41ccf90f42e5af70c79da39bacc3968b
Francescosommellaa <franc.sommella@gmail.com>
2024-06-18 17:33:55 +0200

    add: object array creation for team members

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Creazione di un array di oggetti per rappresentare i membri del team
+const teamMembers = [
+    // Ogni membro del team è rappresentato da un oggetto con proprietà name, role e image
+    { name: 'Wayne Barnett', role: 'Founder & CEO', image: 'img/wayne-barnett-founder-ceo.jpg' },
+    { name: 'Angela', role: 'Chief Editor', image: 'img/angela-caroll-chief-editor.jpg' },
+    { name: 'Walter Gordon', role: 'Office Manager', image: 'img/walter-gordon-office-manager.jpg' },
+    { name: 'Angela Lopez', role: 'Social Media Manager', image: 'img/angela-lopez-social-media-manager.jpg' },
+    { name: 'Scott Estrada', role: 'Developer', image: 'img/scott-estrada-developer.jpg' },
+    { name: 'Barbara Ramos', role:'Graphic Designer', image:'img/barbara-ramos-graphic-designer.jpg'}
+];
+console.log(teamMembers);
